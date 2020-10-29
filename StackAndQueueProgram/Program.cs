882b362413e7916455d485decac6f8aefc26bba3
kartiknawal <kartiknawal.7@gmail.com>
2020-10-29 10:00:01 +0530

    [kartik] Add. initial commit

---
+++ StackAndQueueProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackAndQueueProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to stack and queue DS program!");
+        }
+    }
+}
