cd5d6893718b284be40dc52bbac869421f37114e
kartiknawal <kartiknawal.7@gmail.com>
2020-10-29 10:12:08 +0530

    [kartik] Add. create a queue

--- StackAndQueueProgram/Program.cs
+++ StackAndQueueProgram/Program.cs
@@ -20,6 +20,14 @@ namespace StackAndQueueProgram
             stack.IsEmpty();
             stack.Display();
             Console.ReadKey();
+            Console.WriteLine("\n-------------------------------");
+            Console.WriteLine("Queue:");
+            Queue queue = new Queue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
+            Console.ReadKey();
         }
     }
 }
